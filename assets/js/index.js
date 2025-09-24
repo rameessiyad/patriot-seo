e77dca5bfcd0df00581eb8007e4a13f7784b6b18
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-09-24 12:57:38 +0530

    responsive styles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,15 @@
-const menuToggle = document.getElementById("menu-toggle");
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const closeIcon = document.getElementById("close-icon");
 const mobileMenu = document.getElementById("mobile-menu");
 
-menuToggle.addEventListener("click", () => {
-  mobileMenu.classList.toggle("-translate-x-full");
+hamburgerIcon.addEventListener("click", () => {
+  mobileMenu.classList.remove("-translate-x-full");
+  hamburgerIcon.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
 });
 
+closeIcon.addEventListener("click", () => {
+  mobileMenu.classList.add("-translate-x-full");
+  closeIcon.classList.add("hidden");
+  hamburgerIcon.classList.remove("hidden");
+});
